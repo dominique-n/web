e52c716ce1cd273c880cd45bf0c16e587b72a98c
dom <dominiqn@gmail.com>
2017-03-21 21:27:25 +0100

    take-n-item ok

--- test/web/guardian_test.clj
+++ test/web/guardian_test.clj
@@ -18,65 +18,50 @@
 
 (def api-key (-> creds/portfolio :guardian :api-key))
 
-;(def query-params {:api-key api-key :format "json" :q "brexit" :page-size 50})
-;(def content @(http/get (:content urls) {:query-params query-params}))
-;(keys content)
-;(:status content)
-;(-> content :headers)
-;(-> content :body (json/parse-string true) :response keys)
-;(-> content :body (json/parse-string true) :response :total)
-;(-> content :body (json/parse-string true) :response :pages)
-;(-> content :body (json/parse-string true) )
-;(-> content :body (json/parse-string true) :response :results first)
-;(assert (= (-> content :body (json/parse-string true) :response :pageSize)
-           ;(-> content :body (json/parse-string true) :response :results count))
-        ;"page-size should matche count")
-
-
 (let [path "dev-resources/guardian/"
       http-response (-> (str path "content_http_response.txt") slurp (json/parse-string true))
-      body (-> (str path "content_http_body.txt") slurp (json/parse-string true))] 
+      ;body (-> (str path "content_http_body.txt") slurp (json/parse-string true))] 
+      body (json/parse-string (:body http-response) true)] 
   (with-fake-http [(re-pattern (:content urls)) http-response
                    ;(re-pattern (str "^" (:content urls))) :deny
                    ]
 
     (future-facts "About `apply-quota")
 
-    (facts "About `http-iterate"
+    (facts :http-iterate
+           (facts "About `http-iterate"
                   (count (http-iterate :content {:q "brexit"})) => 263
                   (flatten1 (http-iterate :content {:q "brexit"})) => (n-of map? 13150)
-                  (-> (http-iterate :content {:q "brexit"}) first first)
-                  => (contains {:apiUrl anything} :in-any-order) 
+                  (flatten1 (http-iterate :content {:q "brexit"})) => (has every? #(contains? % :apiUrl))
                   ) 
 
-    (fact "`http-iterate should handle http errors"
+           (fact "`http-iterate should handle http errors"
                  (let [error-msg "http error returned"
                        response {:error error-msg :status "900"}]
                    (http-iterate :content {:q "brexit"}) => (throws Exception error-msg)
-                 (provided
-                 (http/get & anything) => (future {:error error-msg}))))
+                   (provided
+                     (http/get & anything) => (future {:error error-msg}))))
 
-    (fact "`http-iterate should handle non 200 status"
+           (fact "`http-iterate should handle non 200 status"
                  (let [error-msg "status: 401"
                        response {:status 401}]
                    (http-iterate :content {:q "brexit"}) => (throws Exception error-msg)
-                 (provided
-                   (http/get & anything) => (future response))))
+                   (provided
+                     (http/get & anything) => (future response))))
 
-    (fact "`http-iterate should handle non results status"
+           (fact "`http-iterate should handle non results status"
                  (let [response (json/generate-string {:response {:status "not ok"}})]
                    (http-iterate :content {:q "brexit"}) => (throws Exception "api status: not ok")
-                 (provided
-                   (http/get & anything) => (future {:status 200 :body response}))))
+                   (provided
+                     (http/get & anything) => (future {:status 200 :body response}))))
+           )
     )
 
-  (future-facts "Abour `take-n-item"
-                (let [http-iterate (repeat {:body body})]
+  (facts "Abour `take-n-item"
+                (let [http-it (repeat (-> body :response :results))]
                   (take-n-item 3 http-it) => (three-of map?)
                   (take-n-item 3 http-it) => (has every? :apiUrl)
-
-                  (take-n-item 50000 http-it) => (n-of 13101)
-                  (take-n-item 50000 http-it) => (has every? :apiUrl)
+                  (take-n-item 500 http-it) => (n-of #(contains? % :apiUrl) 500)
                   ) 
                 )
   )
